ac5abf6ae86384b9739f1cc87fd336b81e2c68ad
victor <victor@victors-MacBook-Air.local>
2022-12-08 20:51:15 -0500

    worked on main.js and added some jquery

---
+++ echo "# jQuery-Lab" >> README.md
@@ -0,0 +1,7 @@
+echo "# jQuery-Lab" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://git.generalassemb.ly/victor-o/jQuery-Lab.git
+git push -u origin main
--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-//document.getElementById('test').onclick = function() {
-  //  alert('hey');
- // }
- $('#test').click(function() {
-    $(this).hide();
- });
+$('#htcss tbody').on('click', 'button', function() {
+   $(this).closest('tr').remove();
+})
+
+$('#add').on('click', function() {
+   //newskill is what is inside the input value. 0 is where the html element is. value is where we found value of the input.//
+   const newSkill = $('#value')[0].value;
+   // newrow is the way you want to display but there is a whole in the middle to which we add the template literal.
+   const newRow = $(` <tr>
+   <td><button class="btn btn-xs btn-danger">X</button></td>
+   <td class="cas">${newSkill}</td>
+</tr>`)
+// it add the newrow to the tbody 
+$('tbody').append(newRow)
+
+  // console.log($('#value')[0].value);
+   //console.log(newRow);
+})
