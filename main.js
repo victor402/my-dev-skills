a49aee162ca86b66480e442edf0cbb041f2d4e1e
victor <victor@victors-MacBook-Air.local>
2022-12-17 15:28:55 -0500

    add some styles

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-// $('#content tbody').on('click', 'button', function() {
-//    $(this).closest('tr').remove();
-// })
+$('#content tbody').on('click', 'button', function() {
+   $(this).closest('tr').remove();
+})
 
-// $('#add').on('click', function() {
+$('#add').on('click', function() {
    
-//    const newSkill = $('#value')[0].value;
+   const newSkill = $('#value')[0].value;
    
-//    const newRow = $(` <tr>
-//    <td><button class="btn btn-xs btn-danger">X</button></td>
-//    <td class="cas">${newSkill}</td>
-// </tr>`)
+   const newRow = $(` <tr>
+   <td><button class="btn btn-xs btn-danger">X</button></td>
+   <td class="cas">${newSkill}</td>
+</tr>`)
 
-// $('tbody').append(newRow)
+$('tbody').append(newRow)
 
-//    console.log($('#value')[0].value);
-//    console.log(newRow);
-// })
+   console.log($('#value')[0].value);
+   console.log(newRow);
+}) 
 
 
 
